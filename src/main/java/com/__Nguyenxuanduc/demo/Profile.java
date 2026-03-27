6f5a5a2eca9a6efdb95b695da1dad213d8cd5988
xduc695 <xuanducdong2004@gmail.com>
2026-03-27 10:45:34 +0700

    update- deploy.yml and dockerfile3

--- src/main/java/com/__Nguyenxuanduc/demo/Profile.java
+++ src/main/java/com/__Nguyenxuanduc/demo/Profile.java
@@ -1,30 +1,17 @@
 package com.__Nguyenxuanduc.demo;
 
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "profiles")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Profile {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "full_name")
     private String fullName;
-
-    @Column(name = "birth_year")
     private String birthYear;
-
-    @Column(name = "school")
     private String school;
-
-    @Column(name = "role")
     private String role;
-
-    @Column(name = "skills", columnDefinition = "TEXT")
     private String skills;
 }
--- src/main/java/com/__Nguyenxuanduc/demo/ProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.__Nguyenxuanduc.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-}
